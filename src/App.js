141f379e02b14bf624539ec603507e8cabd3454b
watanabehideki <hideki.art.2280@gmail.com>
2022-09-19 17:45:44 +0900

    fetch recipes data from firestore

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./App.css"
 import LoginForm from "./components/LoginForm"
 import FirebaseAuthService from "./FirebaseAuthService"
@@ -7,6 +7,79 @@ import FirebaseFireStoreService from "./FirebaseFireStoreService"
 
 function App() {
   const [user, setUser] = useState(null)
+  const [recipes, setRecipes] = useState()
+
+  useEffect(() => {
+    fetchRecipes()
+      .then((fetchRecipes) => {
+        setRecipes(fetchRecipes)
+      })
+      .catch((error) => {
+        console.error(error.message)
+        throw error
+      })
+  }, [user])
+
+  async function fetchRecipes() {
+
+    const queries = []
+    if(!user) {
+      queries.push({
+        field: 'isPublished',
+        condition: '==',
+        value: true
+      })
+    }
+    let fetchRecipes = []
+    try {
+      const response = await FirebaseFireStoreService.readDocument({collection: "recipes", queries: queries})
+      const newRecipes = response.docs.map((recipeDoc) => {
+        const id = recipeDoc.id
+        const data = recipeDoc.data()
+        data.publishDate = new Date(data.publishDate.seconds * 1000)
+
+        return { ...data, id }
+      })
+
+      fetchRecipes = [...newRecipes]
+    } catch (error) {
+      console.error(error.message)
+      throw error
+    }
+
+    return fetchRecipes
+  }
+
+  async function handleFetchRecipes() {
+    try {
+      setRecipes( await fetchRecipes())
+    } catch (error) {
+      console.error(error.message)
+      throw error
+    }
+  }
+
+  function lookupCategoryLabel(categoryKey) {
+    const categories = {
+      breadsSandwichesAndPizza: "パン、サンドウィッチ、ピザ",
+      eggsAndBreakfast: "たまごと朝食",
+      dessertsAndBakedGoods: "魚とシーフード",
+      veg: "野菜",
+    }
+
+    const label = categories[categoryKey]
+    return label
+  }
+
+  function formatDate(date) {
+    const day = date.getUTCDate()
+    const month = date.getUTCMonth() + 1
+    const year = date.getFullYear()
+    const dateString = `${year}-${month}-${day}`
+
+    return dateString
+  }
+
   FirebaseAuthService.subscribeToAuthChanges(setUser)
 
   async function handleAddRecipe(newRecipe) {
@@ -15,9 +88,7 @@ function App() {
         "recipes",
         newRecipe
       )
-      console.log(`response: ${JSON.stringify(response, null, 3)}`)
-
-      // TODO: firestoreから新しいレシピを取得する
+      handleFetchRecipes()
       alert(`succesfully created a resipe with an ID = ${response.Id}`)
     } catch (error) {
       alert(error.message)
@@ -30,6 +101,29 @@ function App() {
         <LoginForm existingUser={user} />
       </div>
       <div className="main">
+        <div className="center">
+          <div className="recipe-list-box">
+            {recipes && recipes.length > 0 ? (
+              <div className="recipe-list">
+                {recipes.map((recipe) => {
+                  return (
+                    <div className="recipe-card" key={recipe.id}>
+                      {recipe.isPublished ? null : 
+                      <div className="unpublished">未公開</div>}
+                      <div className="recipe-name">{recipe.name}</div>
+                      <div className="recipe-field">
+                        カテゴリー： {recipe.category}
+                      </div>
+                      <div className="recipe-field">
+                        公開日； {formatDate(recipe.publishDate)}
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
+            ) : null}
+          </div>
+        </div>
         {user ? <AddEditRecipeForm handleAddRecipe={handleAddRecipe} /> : null}
       </div>
     </div>
--- src/FirebaseFireStoreService.js
+++ src/FirebaseFireStoreService.js
@@ -6,8 +6,24 @@ const createDocument = (collection, document) => {
   return firestore.collection(collection).add(document)
 }
 
+const readDocument = ({ collection, queries }) => {
+  let collectionRef = firestore.collection(collection)
+  if (queries && queries.length > 0) {
+    for (const query of queries) {
+      // debugger
+      collectionRef = collectionRef.where(
+        query.field,
+        query.condition,
+        query.value
+      )
+    }
+  }
+  return collectionRef.get()
+}
+
 const FirebaseFireStoreService = {
   createDocument,
+  readDocument,
 }
 
 export default FirebaseFireStoreService
